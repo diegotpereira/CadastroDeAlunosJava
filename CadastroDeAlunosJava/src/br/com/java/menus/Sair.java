da451322e00ff2965a70be282bd8daee1b3a813f
Diego <diegoteixeirapereira@hotmail.com>
2021-05-26 11:03:56 -0300

    criação do arquivo classe java sair.

---
+++ CadastroDeAlunosJava/src/br/com/java/menus/Sair.java
@@ -0,0 +1,17 @@
+package br.com.java.menus;
+
+public class Sair extends ItemDeMenu{
+
+	@Override
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return "Sair";
+	}
+
+	@Override
+	public boolean executar() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
