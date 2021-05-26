a17ac851da3366464263170bb6632f1f56000159
Diego <diegoteixeirapereira@hotmail.com>
2021-05-26 11:02:12 -0300

    criação do arquivo classe principal java.

---
+++ CadastroDeAlunosJava/src/br/com/java/Principal.java
@@ -0,0 +1,44 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+import br.com.java.menus.InserirAluno;
+import br.com.java.menus.ItemDeMenu;
+import br.com.java.menus.Sair;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner console = new Scanner(System.in);
+		
+		ItemDeMenu[] principal = new ItemDeMenu[] {
+				
+				new InserirAluno(),
+//				new ListarAlunosPorNome(),
+//				new ConsultarAluno(),
+//				new AtualizarAluno(),
+//				new RemoverAluno(),
+//				new ListarAlunosAprovados(),
+//				new ListarAlunosReprovados(),
+//				new ListarAlunosPorMedia(),
+				new Sair()
+		};
+		
+		boolean sair = false;
+		
+		do {
+			
+			for(int i = 0; i < principal.length; i++) {
+				
+				System.out.println(i + " - " + principal[i].getDescricao());
+			}
+			
+			System.out.print("Qual a opção desejada: ");
+			int opcao = Integer.parseInt(console.nextLine());
+			
+			sair = principal[opcao].executar();
+		}while (!sair);
+	}
+
+}
